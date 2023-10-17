cbbb9e52db64390beeebf39228453d7b09cb2ef1
AlexPolyakovsky <alexander@appcharge.com>
2023-10-17 18:38:26 +0200

    added sessionMetadata field for request/response.

--- src/main/java/com/appcharge/server/models/player/PlayerInfoSyncRequest.java
+++ src/main/java/com/appcharge/server/models/player/PlayerInfoSyncRequest.java
@@ -3,8 +3,11 @@ package com.appcharge.server.models.player;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
 public class PlayerInfoSyncRequest {
     private String playerId;
+    private Map<String, Object> sessionMetadata;
 }
